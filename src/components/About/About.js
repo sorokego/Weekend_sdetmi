f5dc0deb4e64afbaa8c5a8427f91de8286485d3e
EvgeniyaKa <ved-evgeniya@yandex.ru>
2021-05-27 22:36:15 +0300

    Добавила новую картинку на главную страницу

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,16 +6,8 @@ import RectRight from "../../images/icons/RectRight";
 const About = () => {
   return (
     <div className="min-w-320 s:w-full sm:w-full md:w-full xl:w-full mx-auto flex flex-col text-center mb-7">
-      <MainImg/>
-      <div className="flex justify-center items-center w-72 mb-5 mx-auto">
-        <RectLeft/>
-        <h1 className="text-Sea w-full text-xl font-bold px-5">Weekend c детьми</h1>
-        <RectRight/>
-      </div>
-      <span className="w-full px-5 text-base mb-5 text-justify">
-        Это уникальный сервис по поиску различных вариантов досуга с детьми, который приятно удивит
-        Вас своей простотой и информативностью.
-      </span>
+      <h1>Куда сходить с ребёнком в выходной?</h1>
+      <img src="./Rectangle_583.png" alt="imgAbout" />
     </div>
   );
 };
