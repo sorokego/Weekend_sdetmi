f1be0381b467cab832cc8cf90e72b7515773daa5
Egor Sorokin <egor.sorokin@seznam.cz>
2021-06-04 13:24:18 +0200

    Set default city to Yekaterinburg

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -4,56 +4,53 @@ import ChoiceCity from "./ChoiceCity";
 import useVisibilityToggler from "./useVisibilityToggler";
 
 const PopUp = ({ setActive, setCity }) => {
-  const [ChoiseCityComponent, toggleVisibilityCity] = useVisibilityToggler(
-    <ChoiceCity 
-      setActive={setActive} 
-      setCity={setCity} 
-    />,
-    false
-  );
+    const [ChoiseCityComponent, toggleVisibilityCity] = useVisibilityToggler(
+        <ChoiceCity setActive={setActive} setCity={setCity} />,
+        false
+    );
 
-  return (
-    <div className="px-1 relative bg-white">
-      <button
-        className="absolute -top-4 -right-4 border-none bg-transparent cursor-pointer hover:opacity-70 focus:outline-none"
-        onClick={() => setActive(false)}
-      >
-        <IconCloseButton />
-      </button>
-      <div className="flex items-center">
-        <IconMap />
-        <p className="mb-5 mt-1 text-lg font-semibold text-center">
-          Ваш город Москва?
-        </p>
-      </div>
-      {!ChoiseCityComponent ? (
-        <div className="flex justify-between">
-          <button
-            className={
-              "p-2 min-w-14 rounded-10px text-white bg-Sea cursor-pointer hover:bg-Orange hover:shadow-drop focus:outline-none"
-            }
-            onClick={() => {
-              setActive(false);
-              setCity("Москва");
-            }}
-          >
-            Да
-          </button>
-          <button
-            className={
-              "p-2 border border-Sea rounded-10px bg-transparent cursor-pointer hover:border-Orange focus:outline-none"
-            }
-            onClick={() => {
-              toggleVisibilityCity();
-            }}
-          >
-            Выбор города
-          </button>
+    return (
+        <div className="px-1 relative bg-white">
+            <button
+                className="absolute -top-4 -right-4 border-none bg-transparent cursor-pointer hover:opacity-70 focus:outline-none"
+                onClick={() => setActive(false)}
+            >
+                <IconCloseButton />
+            </button>
+            <div className="flex items-center">
+                <IconMap />
+                <p className="mb-5 mt-1 text-lg font-semibold text-center">
+                    Ваш город Екатеринбург?
+                </p>
+            </div>
+            {!ChoiseCityComponent ? (
+                <div className="flex justify-between">
+                    <button
+                        className={
+                            "p-2 min-w-14 rounded-10px text-white bg-Sea cursor-pointer hover:bg-Orange hover:shadow-drop focus:outline-none"
+                        }
+                        onClick={() => {
+                            setActive(false);
+                            setCity("Екатеринбург");
+                        }}
+                    >
+                        Да
+                    </button>
+                    <button
+                        className={
+                            "p-2 border border-Sea rounded-10px bg-transparent cursor-pointer hover:border-Orange focus:outline-none"
+                        }
+                        onClick={() => {
+                            toggleVisibilityCity();
+                        }}
+                    >
+                        Выбор города
+                    </button>
+                </div>
+            ) : null}
+            {ChoiseCityComponent}
         </div>
-      ) : null}
-      {ChoiseCityComponent}
-    </div>
-  );
+    );
 };
 
 export default PopUp;
