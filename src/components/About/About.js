7e0d45c9f7e968fe462bea245a19b905a203c267
EvgeniyaKa <ved-evgeniya@yandex.ru>
2021-06-07 21:42:53 +0300

    Заголовок и картинка на главной стр.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import MainImg from './MainImg';
 const About = () => {
   return (
     <div className="w-80 s:w-full sm:w-full md:w-full xl:w-full mx-auto flex flex-col text-center">
-      <h1 className="sans_mon text-3xl font-bold my-3 mx-6 text-Orange">Куда сходить<br /> с ребёнком<br /> в выходной?</h1>
+      <h1 className="text-3xl font-bold mb-3 mx-6 text-Orange">Куда сходить<br /> с ребёнком<br /> в выходной?</h1>
     <MainImg />
     </div>
   );
